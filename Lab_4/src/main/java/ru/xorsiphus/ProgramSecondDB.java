2568a3381d59b06294e823cc2575cee5b578c794
Evgeniy <superzloyuser@yandex.ru>
2020-12-06 21:30:05 +0700

    Lab_4 report

--- Lab_4/src/main/java/ru/xorsiphus/ProgramSecondDB.java
+++ Lab_4/src/main/java/ru/xorsiphus/ProgramSecondDB.java
@@ -1,6 +1,5 @@
 package ru.xorsiphus;
 
-import org.hibernate.SessionFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
