58b585928b64fcf87ebc24b73b5fb2b9db6a5692
Evgeniy <superzloyuser@yandex.ru>
2020-12-07 22:19:03 +0700

    Auto stash before checking out "HEAD"

--- Lab_4/src/main/java/ru/xorsiphus/dao/second/db/services/CinemasServiceImpl.java
+++ Lab_4/src/main/java/ru/xorsiphus/dao/second/db/services/CinemasServiceImpl.java
@@ -11,6 +11,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Service("cinemasService")
+<<<<<<< Updated upstream
+=======
+//@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+>>>>>>> Stashed changes
 public class CinemasServiceImpl implements AbstractService
 {
     @Autowired
